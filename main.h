0d4c99f24873901f5f448e1098fa23c6b43ab381
SharifAli645 <pandemiaonirica@gmail.com>
2023-03-30 16:48:24 -0700

    feat: prototypes, struct printer

--- main.h
+++ main.h
@@ -1,5 +1,13 @@
 #ifndef ONE_H
 #define ONE_H
+#include <stdarg.h>
+typedef struct printer
+{
+	char *ch;
+	int (*f)(va_list);
+} printer;
 int _printf(const char *format, ...);
 int _putchar(char c);
+int print_char(va_list ptr);
+int print_str(va_list ptr);
 #endif
